2eed12cb5b60233d3f95577dbebaf27ca927ac59
AlexVag <alexis.vaglica95@gmail.com>
2021-06-09 06:05:17 -0300

    Object Pool

---
+++ Assets/Scripts/Engine/ObjectPool/Pool.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ObjectsPool
+{
+    public class Pool<T>
+    {
+        private List<PoolObject<T>> _allObjects;
+        private const int _maxCount = 100;
+        private Func<T> _factoryMethod;
+        private Action<T> _initObject, _finalizeObject;
+
+        
+        public Pool(Func<T> factoryMethod, Action<T> init, Action<T> finalize)
+        {
+            _allObjects = new List<PoolObject<T>>();
+            _factoryMethod = factoryMethod;
+            _initObject = init;
+            _finalizeObject = finalize;
+        }
+
+        public PoolObject<T> GetObject(int count)
+        {
+            for(var i = 0; i < count; i++) {
+                if(!_allObjects[i].isActive) {
+                    _allObjects[i].isActive = true;
+                    return _allObjects[i];
+                }
+            }
+
+            if(_allObjects.Count < _maxCount) {
+                var obj = new PoolObject<T>(_factoryMethod(), _initObject, _finalizeObject);
+                obj.isActive = true;
+                _allObjects.Add(obj);
+                return obj;
+            }
+
+            return null;
+        }
+
+        public void DesactivePoolObject(T obj) {
+            foreach(var pObj in _allObjects) {
+                if(pObj.GetObj.Equals(obj)) {
+                    pObj.isActive = false;
+                    return;
+                }
+            }
+        }
+    }
+}
+
---
+++ Assets/Scripts/Engine/ObjectPool/PoolObject.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ObjectsPool
+{
+    public class PoolObject<T>
+    {
+        #region Attributes
+        private bool _isActive;
+        private Action<T> _init, _finalize;
+        private T _obj;
+        #endregion
+
+        #region Public Methods
+        public PoolObject(T obj, Action<T> init, Action<T> finalize) {
+            _obj = obj;
+            _init = init;
+            _finalize = finalize;
+            _isActive = false;
+        }
+
+        public T GetObj {
+            get {
+                return _obj;
+            }
+        }
+
+        public bool isActive
+        {
+            get {
+                return isActive;
+            }
+
+            set {
+                _isActive = value;
+
+                if (_isActive)
+                    _init?.Invoke(_obj);
+                else
+                    _finalize?.Invoke(_obj);
+
+            }
+        }
+        #endregion
+    }
+}
